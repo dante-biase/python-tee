e921f8249c9b254d8fe2979f8f60b299ffe21deb
dante-biase <dante.biase@outlook.com>
2021-12-21 14:32:08 -0500

    initial commit

---
+++ tee/tee.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+def __read_only_properties(*attrs):
+    def class_rebuilder(cls):
+        "The class decorator"
+        class NewClass(cls):
+            "This is the overwritten class"
+            def __setattr__(self, name, value):
+                if name not in attrs:
+                    pass
+                elif name not in self.__dict__:
+                    pass
+                else:
+                    raise AttributeError("Can't modify {}".format(name))
+                super().__setattr__(name, value)
+        return NewClass
+    return class_rebuilder
+
+
+@__read_only_properties('files', 'mode')
+class Tee:
+
+    def __init__(self, *files: List[str], mode: str = 'w'):
+        if mode not in ['w', 'a']:
+            raise ValueError(f"Invalid mode '{mode}': select from ['w', 'a']")
+        self.files = [open(f, mode) for f in files]
+        self.mode = mode
+
+    def __call__(self, *args, sep: str = None, end: str = None) -> None:
+        for f in self.files:
+            print(*args, sep=sep, end=end, file=f)
+        print(*args, sep=sep, end=end)
+
+
+def tee(files, mode: str = 'w', *args, sep: str = None, end: str = None) -> None:
+    for fname in files:
+        with open(fname, mode) as f:
+            print(*args, sep=sep, end=end, file=f)
+    print(*args, sep=sep, end=end)
+
+__all__ = ["Tee", "tee"]
